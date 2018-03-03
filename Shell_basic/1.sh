28e10a188c4ca5b11dedf808e27b4db68a5ccf2b
WadeZeak <zeak163@gmail.com>
2018-03-03 14:36:31 +0800

    shell_basic

--- Shell_basic/1.sh
+++ Shell_basic/1.sh
@@ -1,12 +1,14 @@
 #/bin/bash
 #
 if [ $1 == '-s' ]; then
+ 	! grep "${2}" /etc/shells &> /dev/null && echo "Invaild shell ."  && exit 7
 elif [ $1 == '--help' ]; then
 	echo "Usage:`basename $0`:w -s shell | --help"
 	exit 0 
 else 
 	echo "Unknow Options."
 	exit 8
+fi
 
 NUMOFUSER=`grep "${2}$" /etc/passwd | wc -l`
 SHELLUSERS=`grep "${2}$" /etc/passwd | cur -d: -fl`
