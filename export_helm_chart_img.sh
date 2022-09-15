ce84a940847922a59249c1b334ba4a87dc5830bc
WadeZeak <1263989504@qq.com>
2022-09-15 11:24:58 +0800

    Update export_helm_chart_img.sh

--- export_helm_chart_img.sh
+++ export_helm_chart_img.sh
@@ -3,6 +3,7 @@ export file_path='/root/helm-chart/'
 #echo $file_path
 export  folder=`ls $file_path`
 #echo $folder
+defaultMirror="docker.m.daocloud.io"
 export mirrorArray=(
 quay.io
 docker.io
@@ -14,34 +15,34 @@ quay.io
 )
 
 for Chart in $folder; do
- 	#imgs=`helm template  "$file_path$Chart" 2> /dev/null  |grep "image:" | sed -e 's/^[ ]*//g'  | sort -n | uniq | sed -e 's/\"//g'`
-	for i in $(helm template  "$file_path$Chart" 2> /dev/null  |grep "image:" | sed -e 's/^[ ]*//g' | sed -e 's/[ ]*$//g' -e 's/"//g' -e 's/\s-//g'| sort -u | uniq ); do
-		if  [[ "$i" =~ "image".* ]]; then
-			continue
-		else
-#			echo $i
-			mirror=${i%%/*}
-			if [[  "${mirrorArray[@]}" =~ $mirror ]]; then
-				tmp=${i#*/}
-				mirrorPrefix=${mirror%.*}
-				mirrorEnd=${mirror##*.}
-				daocloudMirror="${mirror%.*}.m.daocloud.$mirrorEnd"
-#				echo $daocloudMirror
-				i="$daocloudMirror/$tmp"
-#				echo $i
-			else
-				continue
-			fi	
-				imgName=${i##*/}
-				imgVer=${imgName##*:}
-				imgCom=${imgName%%:*}
-#				echo $imgVer
-#				echo $imgCom
-#				docker pull $i 
-				docker save -o  "$file_path$imgCom-$imgVer.tar" $i
-			
-		fi	
+        #imgs=`helm template  "$file_path$Chart" 2> /dev/null  |grep "image:" | sed -e 's/^[ ]*//g'  | sort -n | uniq | sed -e 's/\"//g'`
+        for i in $(helm template  "$file_path$Chart" 2> /dev/null  |grep "image:" | sed -e 's/^[ ]*//g' | sed -e 's/[ ]*$//g' -e 's/"//g' -e 's/\s-//g'| sort -u | uniq ); do
+                if  [[ "$i" =~ "image".* ]]; then
+                        continue
+                else
+#                       echo $i
+                        imgtagPrefix=${i%%/*}
+                        if [[  "${mirrorArray[@]}" =~ $imgtagPrefix ]]; then
+                                tmp=${i#*/}
+                                mirrorPrefix=${imgtagPrefix%.*}
+                                mirrorEnd=${imgtagPrefix##*.}
+                                daocloudMirror="${imgtagPrefix%.*}.m.daocloud.$mirrorEnd"
+#                               echo $daocloudMirror
+                                i="$daocloudMirror/$tmp"
+#                               echo $i
+                        elif [[ "$imgtagPrefix" =~ ^[a-zA-Z][-a-zA-Z0-9]{0,62}([a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$ ]]; then
+                                i="$imgtagPrefix/$i"
+                        fi
+                        imgName=${i##*/}
+                        imgVer=${imgName##*:}
+                        imgCom=${imgName%%:*}
+#                       echo $imgVer
+#                       echo $imgCom
+                        docker pull $i 
+                        docker save -o  "$file_path$imgCom-$imgVer.tar" $i
 
-	done
+                fi
+
+        done
 #echo $folder
 done
